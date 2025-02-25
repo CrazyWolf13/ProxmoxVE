ea2b352b7acc9998909b7586348c82269b64ef47
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-02-25 10:18:11 +0100

    Update web-check-install.sh

--- install/web-check-install.sh
+++ install/web-check-install.sh
@@ -5,7 +5,7 @@
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/lissy93/web-check
 
-source /dev/stdin <<< "https://raw.githubusercontent.com/CrazyWolf13/ProxmoxVE/refs/heads/CrazyWolf13-add-web-check/misc/install.func"
+source /dev/stdin <<< curl -s https://raw.githubusercontent.com/CrazyWolf13/ProxmoxVE/refs/heads/CrazyWolf13-add-web-check/misc/install.func
 color
 verb_ip6
 catch_errors
