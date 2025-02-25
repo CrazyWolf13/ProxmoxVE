1ab2abbc5ac8d4925fa258b3dd4950fe3548b949
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-02-25 09:09:59 +0100

    fix: web-check-install.sh

--- install/web-check-install.sh
+++ install/web-check-install.sh
@@ -13,19 +13,6 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Setting up Node.js Repository"
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
-msg_ok "Set up Node.js Repository"
-
-msg_info "Setup Python3"
-$STD apt-get install -y \
-  python3 \
-  python3-pip \
-rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
-msg_ok "Setup Python3"
-
 msg_info "Installing Dependencies"
 $STD apt-get -y install \ 
   curl \
@@ -41,6 +28,19 @@ $STD apt-get -y install \
   xvfb \
   dbus
 msg_ok "Installed Dependencies"
+
+msg_info "Setting up Node.js Repository"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+msg_ok "Set up Node.js Repository"
+
+msg_info "Setup Python3"
+$STD apt-get install -y \
+  python3 \
+  python3-pip \
+rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
+msg_ok "Setup Python3"
 
 msg_info "Installing Chromium"
 curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /etc/apt/trusted.gpg.d/google-archive.gpg
