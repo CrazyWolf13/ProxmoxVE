1a63a4fcd5569d10a46f54a48fb4c46c2dd2b9d3
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-02-25 14:11:11 +0100

    Update web-check-install.sh

--- install/web-check-install.sh
+++ install/web-check-install.sh
@@ -47,12 +47,6 @@ $STD apt-get update
 $STD apt-get -y install chromium
 msg_ok "Installed Chromium"
 
-msg_info "Setting up Node.js Repository"
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
-msg_ok "Set up Node.js Repository"
-
 msg_info "Installing Node.js"
 $STD apt-get update
 $STD apt-get install -y nodejs
